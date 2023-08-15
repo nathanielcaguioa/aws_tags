55e3fa99b3457e840a88e2304172dd23743f2be2
Nathaniel Caguioa <137973165+nathanielcaguioa@users.noreply.github.com>
2023-08-15 08:52:16 +0800

    Update invoke_create_tag.py

--- invoke_create_tag.py
+++ invoke_create_tag.py
@@ -12,7 +12,28 @@ def mainFunction():
 
 with open('serverlists.txt', 'r') as file:
     servernames = [line.strip() for line in file]
+    
+def get_instance_id_by_name(instance_name):
 
+    response = ec2_client.describe_instances(
+        Filters=[
+            {
+                'Name': 'tag:Name',
+                'Values': [instance_name]
+            }
+        ]
+    )
+    if response['Reservations']:
+            instance_id = response['Reservations'][0]['Instances'][0]['InstanceId']
+            return instance_id
+    else:
+            return None
+
+def create_new_tags(instance_id, new_tagkey, new_tagvalue):
+    ec2_client.create_tags(Resources=[instance_id], Tags=[{'Key': new_tagkey, 'Value': new_tagvalue}])
+    print("Tags added successfully.")
+
+        
 for instance_name in servernames:
 
     region_lookup = {
@@ -26,6 +47,24 @@ for instance_name in servernames:
     }
     aws_region  = region_lookup[instance_name[:4]]
     print(f"'{aws_region}'")
+    
+    def create_session(api_keys, target_env, aws_region):
+        return boto3.Session(
+            aws_access_key_id=inputsecretid,
+            aws_secret_access_key=inputsecretkey,
+            region_name=aws_region
+        )
 
+
+    session = create_session(api_keys, target_env, aws_region)
+    ec2_client = session.client('ec2')
+
+    instance_id = get_instance_id_by_name(instance_name)
+    if instance_id:
+        print(f"Instance ID for '{instance_name}' is: {instance_id}")
+    else:
+        print(f"No instance found with the name '{instance_name}'")
+
+    create_new_tags(instance_id, new_tagkey, new_tagvalue)
 
 mainFunction()
