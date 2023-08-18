3c8d6dff1eeba88c7c02d0027ea5607d0d31ce14
Nathaniel Caguioa <137973165+nathanielcaguioa@users.noreply.github.com>
2023-08-18 12:10:44 +0800

    Update invoke_create_tag.py

--- invoke_create_tag.py
+++ invoke_create_tag.py
@@ -33,14 +33,14 @@ def create_new_tags(instance_id, inputtagkey, inputtagvalue):
     print("Tags added successfully.")
 
     
-    ec2_client = boto3.client('ec2',region_name=aws_region)
+ec2_client = boto3.client('ec2',region_name=aws_region)
+instance_id = get_instance_id_by_name(inputservername)
 
-    instance_id = get_instance_id_by_name(inputservername)
-    if instance_id:
-        print(f"Instance ID for '{inputservername}' is: {instance_id}")
-        create_new_tags(instance_id, inputtagkey, inputtagvalue)
-    else:
-        print(f"No instance found with the name '{inputservername}'")
+if instance_id:
+    print(f"Instance ID for '{inputservername}' is: {instance_id}")
+    create_new_tags(instance_id, inputtagkey, inputtagvalue)
+else:
+    print(f"No instance found with the name '{inputservername}'")
 
     
 
