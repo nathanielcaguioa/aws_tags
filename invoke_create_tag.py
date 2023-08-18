f13af44b8d69741b2f2068621d4bebf3d9d68615
Nathaniel Caguioa <137973165+nathanielcaguioa@users.noreply.github.com>
2023-08-18 11:31:55 +0800

    Update invoke_create_tag.py

--- invoke_create_tag.py
+++ invoke_create_tag.py
@@ -4,18 +4,19 @@ import json
 import os
 inputtagkey = os.environ['tagkey']
 inputtagvalue = os.environ['tagvalue']
+inputservername = os.environ['server']
+inputregion = os.environ['awsRegion']
+
 print(f"'{inputtagkey}' and '{inputtagvalue}'")
 
-with open('serverlists.txt', 'r') as file:
-    servernames = [line.strip() for line in file]
     
-def get_instance_id_by_name(instance_name):
+def get_instance_id_by_name(inputservername):
 
     response = ec2_client.describe_instances(
         Filters=[
             {
                 'Name': 'tag:Name',
-                'Values': [instance_name]
+                'Values': [inputservername]
             }
         ]
     )
@@ -29,29 +30,15 @@ def create_new_tags(instance_id, inputtagkey, inputtagvalue):
     ec2_client.create_tags(Resources=[instance_id], Tags=[{'Key': inputtagkey, 'Value': inputtagvalue}])
     print("Tags added successfully.")
 
-        
-for instance_name in servernames:
-    instance_name = instance_name.upper()
-    region_lookup = {
-        "USEA":"us-east-1",
-        "USWE":"us-west-2",
-        "CACE":"ca-central-1",
-        "EUWE":"eu-west-1",
-        "EUCE":"eu-central-1",
-        "APSP":"ap-southeast-1",
-        "APAU":"ap-southeast-2"
-    }
-    aws_region  = region_lookup[instance_name[:4]]
-    print(f"'{aws_region}'")
     
     ec2_client = boto3.client('ec2',region_name=aws_region)
 
-    instance_id = get_instance_id_by_name(instance_name)
+    instance_id = get_instance_id_by_name(inputservername)
     if instance_id:
-        print(f"Instance ID for '{instance_name}' is: {instance_id}")
+        print(f"Instance ID for '{inputservername}' is: {instance_id}")
         create_new_tags(instance_id, inputtagkey, inputtagvalue)
     else:
-        print(f"No instance found with the name '{instance_name}'")
+        print(f"No instance found with the name '{inputservername}'")
 
     
 
